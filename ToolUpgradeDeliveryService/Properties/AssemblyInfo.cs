66dd2328fe806b4d01ed979cc7505a06cabe6066
Felix-Dev <FelixDev91@gmail.com>
2018-10-14 18:42:11 +0200

    manifest: Updated mod name.

--- ToolUpgradeDeliveryService/Properties/AssemblyInfo.cs
+++ ToolUpgradeDeliveryService/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ToolUpgradeDeliveryService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("StardewValley Mod")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ToolUpgradeDeliveryService")]
+[assembly: AssemblyProduct("StardewMods")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
