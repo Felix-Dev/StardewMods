7d5acd104a97ba3d67ceda704818176c0f916401
Felix <FelixDev91@gmail.com>
2019-08-19 11:09:36 +0200

    Common: Moved <ListExtensions> to new Extensions folder.

--- Common/Helpers/Extensions/ListExtensions.cs
+++ Common/Helpers/Extensions/ListExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StardewMods.Common.Helpers
+namespace FelixDev.StardewMods.Common.Helpers.Extensions
 {
     internal static class ListExtensions
     {
